73c85c3f4233ff54fa08c1543e627c26be294c49
oh971021 <oh971021@naver.com>
2021-07-23 16:58:03 +0900

    study

--- WEB3/myAPP/app_test.go
+++ WEB3/myAPP/app_test.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing" // 요녀석은 xx_test.go 로 만들면 생겨나는 테스팅 모듈
 
 	"github.com/stretchr/testify/assert"
 )
@@ -24,10 +24,12 @@ func TestIndexPathHandler(t *testing.T) {
 	assert := assert.New(t)
 
 	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/", nil) //실제 response를 사용하지 않고 테스팅 응신방법
+	req := httptest.NewRequest("GET", "/", nil) // 실제 response를 사용하지 않고 테스팅 응신방법
 
-	// mux : "/" 경로를 타겟 라우팅(분배) 랜더링 부분
+	// mux : "/" 경로를 타겟 해서 라우팅(분배), 랜더링 부분
+	// mux : 라우터 (분배의 기능, 경로 타겟팅) - 경로에 맞춰서 분배를 해주어라.
 	mux := NewHttpHandler()
+	// 이 단계에서의 핵심 :
 	mux.ServeHTTP(res, req)
 
 	// indexHandler(res, req)
@@ -40,9 +42,13 @@ func TestIndexPathHandler(t *testing.T) {
 
 	// StatusOK와 res.Code가 같은지 비교한다.
 	assert.Equal(http.StatusOK, res.Code)
+
 	// iotil 패키지에 들어있는 readall
+	// iotil 은 err, return 두개의 값을 받는다. 근데 에러는 받지 않기 위해 _로 표시했다.
 	// Readall(response의 body에 뿌려진 정보를 읽어와라)
+	// app.go 의 index handler 에서 render data를 읽어오는 부분이다.
 	data, _ := ioutil.ReadAll(res.Body)
+
 	// 적어놓은 헬로월드와 리스폰스의 바디에서 읽어온 것을 비교한다.
 	assert.Equal("Hello Index!", string(data))
 }
