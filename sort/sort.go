757ed96e2e8769e9b19b656b97d91e764d0e6bf9
Jun-Seok Oh <oh971021@gmail.com>
2021-07-30 12:01:05 +0900

    study Go Sort 7.28

--- sort/sort.go
+++ sort/sort.go
@@ -15,12 +15,14 @@ type Person struct {
 type Friend []Person
 
 // 인터페이스가 가져야 하는 3가지 함수
-func (s Friend) Len() int           { return len(s) }
-func (s Friend) Less(i, j int) bool { return s[i].Age < s[j].Age }
-func (s Friend) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s Friend) Len() int           { return len(s) } = 2
+func (s Friend) Less(i, j int) bool { return s[i].Age < s[j].Age } = 공부 필요
+// 선언해놓은 구조체가 담겨 있는 배열을 건드림.
+func (s Friend) Swap(i, j int)      { s[i], s[j] = s[j], s[i] } = 공부 필요
 
 func main() {
 	p := []Person{
+		// 구조체
 		{"준석", 25},
 		{"호민", 55},
 		{"민영", 28},
@@ -33,6 +35,9 @@ func main() {
 
 	// 인터페이스 타입을 인자로 받는다.
 	fmt.Println("Before sort : ", p)
+	// sort 패키지 안의 Sort 함수를 사용함
+	// Sort() = 매개변수로 인터페이스로 받는다.
+	// 그 받은 인터페이스에 포함되어있는 객체(현재는 배열타입의 Person)를 지정 된 정렬(현재는 Frined[i].Age)을 순서대로 정렬해준다.
 	sort.Sort(Friend(p))
 	fmt.Println("After sort : ", p)
 }
