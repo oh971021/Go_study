d50cd34950a4d6ff8369b372a3ae4f8558bc378e
Junbal <oh971021@gmail.com>
2021-08-12 17:42:41 +0900

    8월 12일 Go Closer

--- Go_Basic/function/closer/second.go
+++ Go_Basic/function/closer/second.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func next() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
+func main() {
+	nextInt := next()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInt := next()
+	fmt.Println(newInt())
 }
