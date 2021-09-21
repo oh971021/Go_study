662074896c62f7f00ba812790110efec5a53b65b
Junseok <oh971021@gmail.com>
2021-09-21 13:15:52 +0000

    Go Review

--- arrangement/Test/Square/main.go
+++ arrangement/Test/Square/main.go
@@ -12,8 +12,20 @@ func add(x, y int) int {
 	return x + y
 }
 
+func mul(x, y int) int {
+	return x / y
+}
+
 func main() {
+
+	var a, b int
+
+	a = 30
+	b = 50
+
 	fmt.Printf("3 * 5 = %d\n", add(3, 5))
 
 	fmt.Printf("9 * 9 = %d\n", square(9))
+
+	fmt.Printf("%d / %d = %d", a, b, mul(a, b))
 }
